0d1173713832b534268e8d720d570c21f953f3c3
Shawn Baker <shawn@frozen.ca>
2016-03-06 00:58:50 -0600

    Added saveImage() and getNavigationBarHeight().

--- app/src/main/java/ca/frozen/rpicameraviewer/classes/Utils.java
+++ app/src/main/java/ca/frozen/rpicameraviewer/classes/Utils.java
@@ -1,17 +1,29 @@
 // Copyright © 2016 Shawn Baker using the MIT License.
 package ca.frozen.rpicameraviewer.classes;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -20,6 +32,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Locale;
 
 import ca.frozen.rpicameraviewer.App;
 import ca.frozen.rpicameraviewer.R;
@@ -498,5 +511,68 @@ public class Utils
 	public static String getNextCameraName(List<Camera> cameras)
 	{
 		return getDefaultCameraName() + " " + (getMaxCameraNumber(cameras) + 1);
+	}
+
+	//******************************************************************************
+	// saveImage
+	//******************************************************************************
+	public static String saveImage(ContentResolver contentResolver, Bitmap source,
+								   String title, String description)
+	{
+		File snapshot = null;
+		Uri url = null;
+		try
+		{
+			// get/create the snapshots folder
+			File pictures = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			File rpi = new File(pictures, App.getStr(R.string.app_name));
+			if (!rpi.exists())
+			{
+				rpi.mkdir();
+			}
+
+			// save the file within the snapshots folder
+			snapshot = new File(rpi, title);
+			OutputStream stream = new FileOutputStream(snapshot);
+			source.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+			stream.flush();
+			stream.close();
+
+			// create the content values
+			ContentValues values = new ContentValues();
+			values.put(MediaStore.Images.Media.TITLE, title);
+			values.put(MediaStore.Images.Media.DISPLAY_NAME, title);
+			if (description != null)
+			{
+				values.put(MediaStore.Images.Media.DESCRIPTION, description);
+			}
+			values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+			values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+			values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+			values.put(MediaStore.Images.ImageColumns.BUCKET_ID, snapshot.toString().toLowerCase(Locale.US).hashCode());
+			values.put(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME, snapshot.getName().toLowerCase(Locale.US));
+			values.put("_data", snapshot.getAbsolutePath());
+
+			// insert the image into the database
+			url = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+		}
+		catch (Exception ex)
+		{
+			return null;
+		}
+
+		// return the URL
+		return (url != null) ? url.toString() : null;
+	}
+
+	//******************************************************************************
+	// getNavigationBarHeight
+	//******************************************************************************
+	public static int getNavigationBarHeight(Context context, int orientation)
+	{
+		Resources resources = context.getResources();
+		String name = orientation == Configuration.ORIENTATION_PORTRAIT ? "navigation_bar_height" : "navigation_bar_height_landscape";
+		int id = resources.getIdentifier(name, "dimen", "android");
+		return (id > 0) ?resources.getDimensionPixelSize(id) : 0;
 	}
 }
