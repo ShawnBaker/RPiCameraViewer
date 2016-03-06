488b0e118e434b9d86ed8fb397c40fdf909cac0b
Shawn Baker <shawn@frozen.ca>
2016-03-06 01:19:50 -0600

    Controls now fade in and out.

--- app/src/main/java/ca/frozen/rpicameraviewer/activities/VideoActivity.java
+++ app/src/main/java/ca/frozen/rpicameraviewer/activities/VideoActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import ca.frozen.rpicameraviewer.classes.Camera;
 import ca.frozen.rpicameraviewer.classes.Utils;
 import ca.frozen.rpicameraviewer.R;
 
-public class VideoActivity extends AppCompatActivity
+public class VideoActivity extends AppCompatActivity implements VideoFragment.OnFadeListener
 {
 	// public constants
 	public final static String CAMERA = "camera";
@@ -20,6 +22,7 @@ public class VideoActivity extends AppCompatActivity
 
 	// instance variables
 	private Camera camera;
+	private FrameLayout frameLayout;
 	private VideoFragment videoFragment;
 
 	//******************************************************************************
@@ -40,10 +43,52 @@ public class VideoActivity extends AppCompatActivity
 		Bundle data = getIntent().getExtras();
 		camera = data.getParcelable(CAMERA);
 
+		// get the frame layout, handle system visibility changes
+		frameLayout = (FrameLayout) findViewById(R.id.video);
+		frameLayout.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+		{
+			@Override
+			public void onSystemUiVisibilityChange(int visibility)
+			{
+				if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+				{
+					videoFragment.startFadeIn();
+				}
+				else
+				{
+				}
+			}
+		});
+
+		// set full screen layout
+		int visibility = frameLayout.getSystemUiVisibility();
+		visibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+		frameLayout.setSystemUiVisibility(visibility);
+
 		// create the video fragment
-		videoFragment = videoFragment.newInstance(camera);
+		videoFragment = videoFragment.newInstance(camera, true);
 		FragmentTransaction fragTran = getSupportFragmentManager().beginTransaction();
 		fragTran.add(R.id.video, videoFragment);
 		fragTran.commit();
+	}
+
+	//******************************************************************************
+	// onStartFadeIn
+	//******************************************************************************
+	@Override
+	public void onStartFadeIn()
+	{
+	}
+
+	//******************************************************************************
+	// onStartFadeOut
+	//******************************************************************************
+	@Override
+	public void onStartFadeOut()
+	{
+		// hide the status and navigation bars
+		int visibility = frameLayout.getSystemUiVisibility();
+		visibility |= View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+		frameLayout.setSystemUiVisibility(visibility);
 	}
 }
